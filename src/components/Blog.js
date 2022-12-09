99fd8809c707c0d50de6c1f28fb1967aa89f348c
andregra <andregra717@gmail.com>
2022-12-09 15:54:14 +0100

    add solution 5.6 and 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,35 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [hide, setHide] = useState(true)
+
+  const show = { display: hide ? 'none' : '' }
+
+  const buttonVisibilityText = hide ? 'view' : 'hide'
+
+  const toggleVisibility = () => {
+    setHide(!hide)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{buttonVisibilityText}</button> 
+        <div style={show}>
+          <div>{blog.url}</div>
+          <div>likes: {blog.likes} <button>like</button></div>
+          <div>{blog.user.username}</div>
+        </div>
+    </div>
+  )
+}
 
 export default Blog
