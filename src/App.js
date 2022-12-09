29a89ed1f5076f20256042e1a57b0e4729b1b9f8
andregra <andregra717@gmail.com>
2022-12-09 16:40:13 +0100

    add solution 5.9

--- src/App.js
+++ src/App.js
@@ -142,7 +142,7 @@ const App = () => {
               blogs={blogs}
             />
           </Toggable>
-          {blogs.map(blog =>
+          {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
         </div>
