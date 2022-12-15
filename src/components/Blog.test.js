1de412081a8a60610c2c9ba7335a1d35d078ad65
andregra <andregra717@gmail.com>
2022-12-15 14:58:22 +0100

    add solution 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', async () => {
+describe('<Blog />', () => {
+
   const blog = {
     title: 'Component testing is done with react-testing-library',
     author: 'Someone',
@@ -17,12 +19,30 @@ test('renders content', async () => {
     }
   }
 
-  render(<Blog blog={blog} />)
 
-  const title = screen.getByText('Component testing is done with react-testing-library', { exact: false })
-  const author = screen.getByText('Someone', { exact: false })
-  const divToggable = screen.getAllByTestId('toggableContent')
-  expect(title).toBeDefined()
-  expect(author).toBeDefined()
-  expect(divToggable[0]).toHaveStyle('display: none')
+  beforeEach(() => {
+    render(
+      <Blog blog={blog} />
+    )
+  })
+
+  test('renders content', async () => {
+    const title = screen.getByText('Component testing is done with react-testing-library', { exact: false })
+    const author = screen.getByText('Someone', { exact: false })
+    const divToggable = screen.getAllByTestId('toggableContent')
+    expect(title).toBeDefined()
+    expect(author).toBeDefined()
+    expect(divToggable[0]).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view', { exact: false })
+    await user.click(button)
+
+    const divToggable = screen.getAllByTestId('toggableContent')
+
+    expect(divToggable[0]).toHaveStyle('display: block')
+  })
+
 })
